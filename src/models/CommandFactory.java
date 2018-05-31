cf4bef8a70a3878037f6b980a96af2698c654d33
kurumin <andrebardo@gmail.com>
2018-05-31 19:14:23 -0300

    Code from Asus notebook

--- src/models/CommandFactory.java
+++ src/models/CommandFactory.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 /**
@@ -18,5 +13,4 @@ public class CommandFactory {
         name.setPrimaryDescription("set up a new name for your character");
         return name;
     }
-    
 }
